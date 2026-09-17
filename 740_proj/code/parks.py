66dabfc8819c31dfa289ad68614dd1fb3ee2d2aa
hannah <hannahle249@gmail.com>
2026-09-17 12:57:03 -0700

    more data

--- 740_proj/code/parks.py
+++ 740_proj/code/parks.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-parks = pd.read_csv('parks_name.csv')
+parks = pd.read_csv('740_proj/data/parks_name.csv')
 
 print(parks.head(5))
 
